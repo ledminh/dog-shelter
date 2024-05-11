570e0580deb75df802e5e3ce4e22c3b3d1974d2d
Minh Le <minh.le@ledminh.dev>
2024-05-10 22:22:10 -0400

    Add Link to Logo

--- src/components/Layout/Logo/index.tsx
+++ src/components/Layout/Logo/index.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import { DogIcon } from "../../../assets/icons";
 import { logo } from "@/config";
 
 export default function Logo() {
   const { line1, line2 } = logo;
   return (
-    <div className="flex gap-2">
+    <Link href="/" className="flex gap-2">
       <div className="w-14 h-14 bg-primary-2 text-white rounded-full p-1">
         <DogIcon />
       </div>
@@ -12,6 +13,8 @@ export default function Logo() {
         <h1 className="text-secondary text-lg font-bold">{line1}</h1>
         <h1 className="text-primary-2 text-lg font-bold">{line2}</h1>
       </div>
-    </div>
+    </Link>
   );
 }
+
+// Updated
