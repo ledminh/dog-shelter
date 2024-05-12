96b34c1e562ca1f49cb260e63db301b166573718
Minh Le <minh.le@ledminh.dev>
2024-05-12 00:56:04 -0400

    Add Prev and Next handle

--- src/app/puppies/page.tsx
+++ src/app/puppies/page.tsx
@@ -1,16 +1,34 @@
+"use client";
+
 import MainComponent from "@/components/Puppies/MainComponent";
-import CircleButtonLink from "@/components/commons/CircleButtonLink";
 import { dogs } from "@/data";
+import { useState } from "react";
 
 export default function PuppiesPage() {
+  const [curDogI, setCurDogI] = useState(0);
+
+  const handleNext = () => {
+    setCurDogI((prev) => (prev + 1) % dogs.length);
+  };
+
+  const handlePrev = () => {
+    setCurDogI((prev) => (prev - 1 + dogs.length) % dogs.length);
+  };
+
   return (
     <>
-      <MainComponent dog={dogs[0]} />
-      <div className="flex gap-2">
-        <button className="bg-gray-400 px-4 py-2 font-semibold rounded-3xl hover:bg-gray-500">
+      <MainComponent dog={dogs[curDogI]} />
+      <div className="flex gap-2 absolute bottom-2 right-2">
+        <button
+          className="bg-gray-400 px-4 py-2 font-semibold rounded-3xl hover:bg-gray-500"
+          onClick={handlePrev}
+        >
           PREV
         </button>
-        <button className="bg-gray-400 px-4 py-2 font-semibold rounded-3xl hover:bg-gray-500">
+        <button
+          className="bg-gray-400 px-4 py-2 font-semibold rounded-3xl hover:bg-gray-500"
+          onClick={handleNext}
+        >
           NEXT
         </button>
       </div>
